c17d96c5541d3efbd786b4e5f7444734f5f5d5db
Koushik <koushikrp26@gmail.com>
2021-12-05 01:14:19 +0530

    [feat] mark eight done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+var sportDictionary = {
+	"🏏": "Cricket",
+	"⚽": "Football",
+	"🎿": "Skiing",
+	"🎾": "Tennis",
+	"🏑": "Hockey",
+	"🏸": "Badminton",
+	"🥊": "Boxing",
+	"⚾": "Baseball",
+	"🎳": "Bowling",
+	"🏐": "Volleyball",
+	"": "The Sport Name will be displayed here",
+};
+var emojisWeKnow = Object.keys(sportDictionary);
+
+export default function App() {
+	var [sportName, setName] = useState("The Sport Name will be displayed here");
+	function sportInputHandler(event) {
+		var userInput = event.target.value;
+		var meaning = sportDictionary[userInput];
+		if (meaning === undefined) {
+			meaning = "We don't have this in our database";
+		}
+		setName(meaning);
+	}
+	function handleClick(emoji) {
+		setName(sportDictionary[emoji]);
+	}
+	return (
+		<div className="App">
+			<h1 className="App-header"> Sport Interpreter </h1>
+			<h2>
+				Input the Sport Emoji to know it's Name.
+				<br /> Or Click below Emojis to know what they represent
+			</h2>
+			<div id="id1">
+				<input id="input1" onChange={sportInputHandler} />
+			</div>
+			<div id="output">
+				Sport Name: <br /> <span style={{ color: "green", fontSize: "larger" }}> {sportName}</span>{" "}
+			</div>
+
+			<h2>Emojis we know</h2>
+			<ul>
+				{emojisWeKnow.map((emoji) => {
+					return (
+						<span
+							onClick={() => handleClick(emoji)}
+							style={{ fontSize: "3rem", padding: "0.5rem", cursor: "pointer" }}
+						>
+							{emoji}
+						</span>
+					);
+				})}
+			</ul>
+		</div>
+	);
 }
-
-export default App;
